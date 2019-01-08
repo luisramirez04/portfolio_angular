595109c3c2c8c051673dd02fb41e16d17d059c31
Luis <luis@Luiss-MacBook-Pro.local>
2019-01-08 06:02:10 -0800

    structurally and stylistically matches non-Angular version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { InterestsComponent } from './interests/interests.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ResumeComponent } from './resume/resume.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "home", component: HomeComponent },
+  { path: "interests", component: InterestsComponent },
+  { path: "projects", component: ProjectsComponent },
+  { path: "resume", component: ResumeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { InterestsComponent } from './interests/interests.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ResumeComponent } from './resume/resume.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    InterestsComponent,
+    ProjectsComponent,
+    ResumeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
